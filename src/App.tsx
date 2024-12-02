b0cf5f72d9654ae0971cbad59d2442fb79fda39d
eliks-ind <20fantomas09@gmail.com>
2024-12-03 02:12:55 +0400

    Migration on TS and bug fixes

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface User {
+  name: string;
+  surname: string;
+}
+
+const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState<string>("");
+  const [surname, setSurname] = useState<string>("");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("Network error");
+        }
+
+        interface ApiUser {
+          name: string;
+        }
+
+        const data: ApiUser[] = await response.json();
+
+        const firstThreeUsers = data.slice(0, 3).map((user) => {
+          const partsOfName = user.name.split(" ");
+          return {
+            name: partsOfName[0],
+            surname: partsOfName[1],
+          };
+        });
+        setUsers(firstThreeUsers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const addUser = () => {
+    if (name && surname) {
+      const newUser = { name, surname };
+      setUsers((prevUsers) => [...prevUsers, newUser]);
+      setName("");
+      setSurname("");
+    } else {
+      alert("Please fill both fields.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Ultimate Users List</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.name} {user.surname}
+          </li>
+        ))}
+      </ul>
+      <div className="inputForms">
+        <h2>Add a User</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br></br>
+        <input
+          type="text"
+          placeholder="Surname"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+        />
+        <br></br>
+        <button onClick={addUser}>Add User</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
